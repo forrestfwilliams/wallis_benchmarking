20aba3b4025ff78b1da62c4fef9aad070b5167e2
Alex Gardner <32276930+alex-s-gardner@users.noreply.github.com>
2022-06-16 15:04:49 -0700

    I've cleaned up the code a bit and added a benchmark for mapwindow

--- wallis_filter.jl
+++ wallis_filter.jl
@@ -1,19 +1,32 @@
 using Statistics
+using ImageFiltering
+
 w = 5;
 x = 7000;
 y = x;
 I = rand(x,y)
 F = zeros(x,y)
-# now using 1 core [time = 15s for x = 5000]
+
+# define wallis filter
+function wallis(chip)
+    chip_mean = mean(chip)
+    kernel_sum = length(chip)
+    chip_std = sqrt(sum((chip .- chip_mean).^2)/kernel_sum)
+    return out = (chip[ceil(Int,kernel_sum/2)] - chip_mean) / chip_std
+end
+
+# now using 1 core [time = 27s for x = 7000]
 ww = floor(Int,w/2)
 @time for i = (ww+1):1:(x-ww-1), j = (ww+1):1:(x-ww-1)
-    chip = I[(i-ww):(i+ww), (j-ww):(j+ww)];
-    F[i,j] = (I[i,j] - mean(chip))/std(chip[:])
+    F[i,j] = wallis(I[(i-ww):(i+ww), (j-ww):(j+ww)])
 end
-# now using 4 threads [time = 7s for x = 5000]
+
+# now using mutiplt threads (if Julia setup to use multiple threads) [time = 11s for x = 7000]
 @time Threads.@threads for i = (ww+1):1:(x-ww-1)
     for j = (ww+1):1:(x-ww-1)
-    chip = I[(i-ww):(i+ww), (j-ww):(j+ww)];
-    F[i,j] = (I[i,j] - mean(chip))/std(chip[:])
+        F[i,j] = wallis(I[(i-ww):(i+ww), (j-ww):(j+ww)])
     end
 end
+
+# now using mapwindow and mutiplt threads (if Julia setup to use multiple threads) [time = 9s for x = 7000]
+@time a = mapwindow(wallis, I, (w,w));
