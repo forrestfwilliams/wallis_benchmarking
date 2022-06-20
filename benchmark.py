da8cd2051b1787035bf5121c0fcff9e27cdc9be2
Forrest Williams <ffwilliams2@alaska.edu>
2022-06-20 12:28:19 -0500

    update with working std filter

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.ndimage
 import scipy.ndimage as ndimage
 import cv2
 import dask.array as da
@@ -23,70 +24,102 @@ def time_function(fn, fn_args, n=1):
     for i in range(n):
         start = time.perf_counter()
         result = fn(**fn_args)
-        print(type(result))
-        print(result.shape)
         end = time.perf_counter()
         times.append(end - start)
 
     return np.mean(times)
 
 
-def scipy_wallis(array, filter_kernel):
-    kernel_sum = np.sum(filter_kernel)
-    mean = ndimage.uniform_filter(array, kernel.shape[0])
-    squared_mean = ndimage.uniform_filter(mean ** 2, kernel.shape[0])
-    std = np.sqrt((squared_mean - mean) ** 2) * np.sqrt(kernel_sum / (kernel_sum - 1.0))
+def scipy_std(array, filter_width):
+    '''
+    https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+    http://cpsc.yale.edu/sites/default/files/files/tr222.pdf
+    '''
+    n = filter_width ** 2
+    shifted = array - np.mean(array)
+    conv_sum = ndimage.convolve(shifted, np.ones((filter_width, filter_width)))
+    conv_squared_sum = ndimage.convolve(shifted ** 2, np.ones((filter_width, filter_width)))
+    variance = (conv_squared_sum - ((conv_sum ** 2) / n)) / n
+    std = np.sqrt(variance)
+    return std
+
+
+def scipy_wallis(array, filter_width):
+    std = scipy_std(array, filter_width)
+    mean = ndimage.uniform_filter(array, filter_width)
 
     out_array = (array - mean) / std
     return out_array
 
 
-def dask_wallis(array, filter_kernel):
-    with dask.config.set(scheduler='thread'): #change to sychronous for single thread
-        kernel_sum = np.sum(filter_kernel)
-        da_array = da.from_array(array, chunks=(4, 4))
-        mean = da_array.map_overlap(lambda x: ndimage.uniform_filter(x, kernel.shape[0]), depth=kernel.shape[0],
+def cv2_std(array, filter_width):
+    n = filter_width ** 2
+    shifted = array - np.mean(array)
+    filter_kernel = np.ones((filter_width, filter_width))
+    conv_sum = cv2.filter2D(shifted, -1, filter_kernel, borderType=cv2.BORDER_REFLECT)
+    conv_squared_sum = cv2.filter2D(shifted ** 2, -1, filter_kernel, borderType=cv2.BORDER_REFLECT)
+
+    variance = (conv_squared_sum - ((conv_sum ** 2) / n)) / n
+    std = np.sqrt(variance)
+    return std
+
+
+def cv2_wallis(array, filter_width):
+    n = filter_width ** 2
+    filter_kernel = np.ones((filter_width, filter_width))
+
+    std = cv2_std(array, filter_width)
+    mean = cv2.filter2D(array, -1, filter_kernel, borderType=cv2.BORDER_REFLECT) / n
+
+    out_array = (array - mean) / std
+    return out_array
+
+
+def dask_std(array, filter_width):
+    n = filter_width ** 2
+    shifted = array - array.mean()
+    filter_kernel = np.ones((filter_width, filter_width))
+    # da_array = da.from_array(array, chunks=(4, 4))
+    conv_sum = shifted.map_overlap(lambda x: ndimage.convolve(x, filter_kernel), depth=filter_width, boundary='reflect')
+    conv_squared_sum = (shifted ** 2).map_overlap(lambda x: ndimage.convolve(x, filter_kernel), depth=filter_width,
+                                                  boundary='reflect')
+
+    variance = (conv_squared_sum - ((conv_sum ** 2) / n)) / n
+    std = np.sqrt(variance)
+    return std
+
+
+def dask_wallis(array, filter_width):
+    with dask.config.set(scheduler='threads'):  # change to sychronous for single thread
+        da_array = da.from_array(array)
+        n = filter_width ** 2
+
+        std = cv2_std(array, filter_width)
+        mean = da_array.map_overlap(lambda x: ndimage.uniform_filter(x, filter_width), depth=filter_width,
                                     boundary='reflect')
-        squared_mean = mean ** 2
-        squared_mean = squared_mean.map_overlap(lambda x: ndimage.uniform_filter(x, kernel.shape[0]), depth=kernel.shape[0],
-                                                boundary='reflect')
-
-        std = np.sqrt((squared_mean - mean) ** 2) * np.sqrt(kernel_sum / (kernel_sum - 1.0))
-
         out_array = (array - mean) / std
         out_array = out_array.compute()
     return out_array
 
 
-def cv2_wallis(array, filter_kernel):
-    kernel_sum = np.sum(filter_kernel)
-    mean = cv2.filter2D(array, -1, filter_kernel, borderType=cv2.BORDER_CONSTANT) / kernel_sum
-    squared_mean = cv2.filter2D(mean ** 2, -1, kernel, borderType=cv2.BORDER_CONSTANT) / kernel_sum
+if __name__ == '__main__':
+    n_runs = 1
+    data_width = 7_000  # rough size of landsat 7 scene (not panchromatic)
+    kernel_width = 5
 
-    std = np.sqrt((squared_mean - mean) ** 2) * np.sqrt(kernel_sum / (kernel_sum - 1.0))
+    test_data = create_data((data_width, data_width))
+    in_data = {'array': test_data, 'filter_width': kernel_width}
 
-    out_array = (array - mean) / std
-    return out_array
+    print('starting scipy')
+    exec_time = time_function(scipy_wallis, in_data, n_runs)
+    print(f'Scipy took {exec_time:.03f}\n')
 
+    print('starting CV2')
+    exec_time = time_function(cv2_wallis, in_data, n_runs)
+    print(f'CV2 took {exec_time:.03f}\n')
 
-n_runs = 1
-data_width = 7_000  # rough size of landsat 7 scene (not panchromatic)
-kernel_width = 5
+    print('starting dask')
+    exec_time = time_function(cv2_wallis, in_data, n_runs)
+    print(f'Dask took {exec_time:.03f}\n')
 
-test_data = create_data((data_width, data_width))
-kernel = np.ones((kernel_width, kernel_width))
-in_data = {'array': test_data, 'filter_kernel': kernel}
-
-print('starting scipy')
-exec_time = time_function(scipy_wallis, in_data, n_runs)
-print(f'Scipy took {exec_time:.03f}\n')
-
-print('starting CV2')
-exec_time = time_function(cv2_wallis, in_data, n_runs)
-print(f'CV2 took {exec_time:.03f}\n')
-
-print('starting dask')
-exec_time = time_function(cv2_wallis, in_data, n_runs)
-print(f'Dask took {exec_time:.03f}\n')
-
-print('done!')
+    print('done!')
---
+++ test_benchmark.py
@@ -0,0 +1,71 @@
+import pytest
+import benchmark
+import numpy as np
+
+
+def generate_data():
+    image = np.ones((5, 5)) * 20
+    image[:2, :2] += np.ones((2, 2)) * 30
+    return image
+
+
+def test_scipy_std_value():
+    width = 3
+    image = generate_data()
+    subset = image[1:4, 1:4]
+    std = np.std(subset.flatten())
+    assert np.round(std, 5) == np.round(benchmark.scipy_std(image, width)[2, 2], 5)
+
+
+def test_scipy_wallis_value():
+    width = 3
+    image = generate_data()
+    subset = image[1:4, 1:4]
+    std = np.std(subset.flatten())
+    mean = np.mean(subset.flatten())
+    wallis = (image[2, 2] - mean) / std
+    assert np.round(wallis, 5) == np.round(benchmark.scipy_wallis(image, width)[2, 2], 5)
+
+
+def test_cv2_std_value():
+    width = 3
+    image = generate_data()
+    subset = image[1:4, 1:4]
+    std = np.std(subset.flatten())
+    assert np.round(std, 5) == np.round(benchmark.cv2_std(image, width)[2, 2], 5)
+
+
+def test_cv2_wallis_value():
+    width = 3
+    image = generate_data()
+    subset = image[1:4, 1:4]
+    std = np.std(subset.flatten())
+    mean = np.mean(subset.flatten())
+    wallis = (image[2, 2] - mean) / std
+    assert np.round(wallis, 5) == np.round(benchmark.cv2_wallis(image, width)[2, 2], 5)
+
+
+def test_dask_std_value():
+    import dask
+    import dask.array as da
+    width = 3
+    image = generate_data()
+    subset = image[1:4, 1:4]
+    std = np.std(subset.flatten())
+
+    with dask.config.set(scheduler='threads'):  # change to sychronous for single thread
+        da_image = da.from_array(image)
+        da_std = benchmark.dask_std(da_image, width)
+        da_std.compute()
+
+    assert np.round(std, 5) == np.round(da_std[2, 2], 5)
+
+
+def test_scipy_dask_value():
+    width = 3
+    image = generate_data()
+    subset = image[1:4, 1:4]
+    std = np.std(subset.flatten())
+    mean = np.mean(subset.flatten())
+    wallis = (image[2, 2] - mean) / std
+    assert np.round(wallis, 5) == np.round(benchmark.dask_wallis(image, width)[2, 2], 5)
